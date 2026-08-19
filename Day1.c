6e3d8355d2d0f44df6a7dd6bc6efe2e5541c9e22
Sankalp-0610 <sankalp.34613@stu.upes.ac.in>
2026-08-19 23:07:09 +0530

    Add files via upload

---
+++ Day1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+
+    int a,b;
+
+    printf("Enter first number =");
+    scanf("%d", &a);
+
+    printf("Enter second number =");
+    scanf("%d", &b);
+
+    int sum = a+b;
+
+    printf("The sum is %d", sum);
+
+    return 0;
+
+
+}
